539964abe1083838403ebdb5e16022e8bb1cd4b1
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-13 19:47:39 -0300

    Add project files.

---
+++ CalculadoraPOO.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculadoraPOO.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
