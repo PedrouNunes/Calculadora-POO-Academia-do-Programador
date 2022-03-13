57df1ec93124b5fb5573e29cb506742f15ad6c49
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-13 20:19:14 -0300

    Falta o Histórico

---
+++ CalculadoraPOO.ConsoleApp/Calculadora.cs
@@ -0,0 +1,39 @@
+﻿namespace CalculadoraPOO.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class Calculadora
+        {
+            double resultadoOperacao = 0;
+
+            public double Soma(double primeiroNumero, double segundoNumero)
+            {
+                resultadoOperacao = primeiroNumero + segundoNumero;
+                return resultadoOperacao;
+            }
+
+            public double Subtracao(double primeiroNumero, double segundoNumero)
+            {
+                resultadoOperacao = primeiroNumero - segundoNumero;
+                return resultadoOperacao;
+            }
+
+            public double Divisao(double primeiroNumero, double segundoNumero)
+            {
+                resultadoOperacao = primeiroNumero / segundoNumero;
+                return resultadoOperacao;
+            }
+
+            public double Multiplicacao(double primeiroNumero, double segundoNumero)
+            {
+                resultadoOperacao = primeiroNumero * segundoNumero;
+                return resultadoOperacao;
+            }
+                            
+}
+        }
+            }
+
+           
+    
+
--- CalculadoraPOO.ConsoleApp/Program.cs
+++ CalculadoraPOO.ConsoleApp/Program.cs
@@ -2,11 +2,64 @@
 
 namespace CalculadoraPOO.ConsoleApp
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Calculadora");
+                Console.WriteLine("Digite 1 para somar");
+
+                Console.WriteLine("Digite 2 para subtrair");
+
+                Console.WriteLine("Digite 3 para multiplicar");
+
+                Console.WriteLine("Digite 4 para dividir");
+
+                Console.WriteLine("Digite s para sair");
+
+                Console.Write("Resposta: ");
+                opcao = Console.ReadLine();
+
+                if (opcao == "s")
+                    break;
+
+                Console.WriteLine();
+
+                Console.Write("Digite o primeiro número: ");
+                double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.Write("Digite o segundo número: ");
+                double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                Calculadora conta = new Calculadora();
+
+                Console.WriteLine();
+
+                if (opcao == "1")
+                {
+                    Console.Write("Resultado da soma: " + Math.Round(conta.Soma(primeiroNumero, segundoNumero), 2));
+                }
+                if (opcao == "2")
+                {
+                    Console.Write("Resultado da subtração: " + Math.Round(conta.Subtracao(primeiroNumero, segundoNumero), 2));
+                }
+                if (opcao == "3")
+                {
+                    Console.Write("Resultado da multiplicação: " + Math.Round(conta.Multiplicacao(primeiroNumero, segundoNumero), 2));
+                }
+                if (opcao == "4")
+                {
+                    Console.Write("Resultado da divisão: " + Math.Round(conta.Divisao(primeiroNumero, segundoNumero), 2));
+                }
+
+                Console.ReadLine();
+            } while (opcao == "1" || opcao == "2" || opcao == "3" || opcao == "4");
         }
     }
 }
